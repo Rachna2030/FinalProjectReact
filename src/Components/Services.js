f212e043bf8c4b8f30b9cb3777a89ccf86a7a8c5
Harpreet8710 <148901217+Harpreet8710@users.noreply.github.com>
2024-07-06 22:10:09 -0400

    Seventh commit

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,16 +1,19 @@
 import React from "react";
+import './Services.css';
 
 function Services() {
     return (
         <div className="services">
             <h1>Our Services</h1>
-            <p>We run a general family practice, and have a clinical focus in Female and kids Wellness,, Digestive health, Environmental health, Skin health and Allergies.</p>
+            <p1>We run a general family practice, and have a clinical focus in Female and kids Wellness, 
+                Digestive health, Environmental health, Skin health and Allergies.</p1>
             <br></br>
             <br></br>
             <div className="concern">
-            <h2>COMMON CONCERNS</h2>
-            <p>If you’re wondering how I might assist you, the following are examples of what I have consulted and treated people for in the past. This includes, but is not limited to:</p>
-            <ul>
+            <h3>COMMON CONCERNS</h3>
+            <p2>If you're curious about how our team can assist you, here are some examples of
+                 issues we've consulted on and treated in the past.</p2>
+            <ol>
                 <li>Gastrointestinal diseases (IBS, IBD (Colitis/Crohns), Reflux, Leaky Gut, Bloating, Constipation)</li>
                 <li>Hormone imbalance</li>
                 <li>Sleep issues/insomnia</li>
@@ -20,19 +23,18 @@ function Services() {
                 <li>kids health checkups</li>
                 <li>full body checkup</li>   
                 <li>yoga,health fitness classes</li>
-            </ul>
+            </ol>
             </div>
-            <div className="contact">
+            <br></br>
+            <br></br>
+            <div className="more-services">
                 <h2>Available Services</h2>
                 <ul>
                     <li>General Checkup</li>
                     <li>Eye Checkup</li>
-                    <li>ENT Checkup</li>
                     <li>Heart Checkup</li>
                     <li>Diabetes Checkup</li>
-                    <li>Online access to lab results</li>
-                    <li>Consultation</li>
-                    <li>Medicines</li>
+                   <li>Medicines</li>
                     <li>Email contact</li>
                     <li>Individualized treatment, including nutritional, lifestyle, exercise, stress management plans and support</li>
                 </ul>
